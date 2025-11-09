0ee203e33006cdfe2559b284e40745e2276bee47
Pedro Eusebio <pedromanueleu@gmail.com>
2025-11-09 11:34:54 -0400

    first commit

--- models/Testimoniales.js
+++ models/Testimoniales.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import db from "../config/db.js";
 
-export const Testimonial = db.define('Testimoniales',{
+export const Testimonial = db.define('testimoniales',{
     nombre: {
     type: Sequelize.STRING
     },
